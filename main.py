6df14f18eae10d22c769b1ed8e1f8116deeb3c18
morozov.s <morozov.s@gazgolder.ru>
2025-03-22 23:45:44 +0300

    correct hook terminate from terminate process

--- main.py
+++ main.py
@@ -1,3 +1,12 @@
+"""
+Катапульта на процессах.
+
+Проблема катапульты на тредах в том, что чтобы дропнуть треду, которая
+следит за terminate статусом, нужно передать ей эвент. Передать можно, но
+на стороне треды нужно писать обработчик. А если операция получения данных
+о терминации блокирующая, то до обработчика мы не дойдем.
+"""
+
 import logging
 import multiprocessing as mp
 import os
@@ -28,14 +37,14 @@ def consumer_start():
     return int(val)
 
 
-def consumer_terminate():
+def consumer_terminate(pipe_conn):
     """
     """
     logging.info('start terminate process')
     while True:
         val = redis_consumer('terminate')
         if val == 'stop':
-            break
+            pipe_conn.send("stop")
     logging.info('end terminate process')
     
 
@@ -43,29 +52,34 @@ def catapult():
     """
     """
     logging.info('catapult started')
-    process, terminate = None, None
+    process, terminate, pipe_from_main = None, None, None
     while True:
         if process is None:
             time_process = consumer_start()
             process = mp.Process(target=worker, kwargs={'sec': int(time_process)}, daemon=True)
-            terminate = mp.Process(target=consumer_terminate, daemon=True)
+            
+            pipe_from_main, pipe_to_terminate = mp.Pipe()
+
+            terminate = mp.Process(target=consumer_terminate, args=(pipe_to_terminate,), daemon=True)
             process.start()
             terminate.start()
         else:
             while True:
-                if not terminate.is_alive():
+                if pipe_from_main is not None and pipe_from_main.poll(1):
                     logging.info('STOP VIA TERMINATE')
+                    os.kill(process.pid, signal.SIGTERM)  
+
+                    os.kill(terminate.pid, signal.SIGTERM)    
                     terminate.join()
-                    os.kill(process.pid, signal.SIGTERM)    
                     process.join()
-                    process, terminate = None, None
+                    process, terminate, pipe_from_main = None, None, None
                     break
                 elif not process.is_alive():
                     logging.info('STOP VIA END WORKER')
-                    process.join()
                     os.kill(terminate.pid, signal.SIGTERM)
-                    terminate.join()  
-                    process, terminate = None, None
+                    terminate.join()
+                    process.join()
+                    process, terminate, pipe_from_main = None, None, None
                     break
 
 
